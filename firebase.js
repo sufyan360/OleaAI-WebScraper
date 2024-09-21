6d73af9ff6013208df60097fb8df0d0345191242
sufyan360 <117491069+sufyan360@users.noreply.github.com>
2024-09-21 16:35:11 -0500

    remove key

--- firebase.js
+++ firebase.js
@@ -7,7 +7,7 @@ import { getAnalytics } from "firebase/analytics";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyANI-SvxqDpQHoVkLtK96WLR2JXr_yQE20",
+  apiKey: process.env("GOOGLE_API_KEY"),
   authDomain: "oleampoxscraper.firebaseapp.com",
   projectId: "oleampoxscraper",
   storageBucket: "oleampoxscraper.appspot.com",
